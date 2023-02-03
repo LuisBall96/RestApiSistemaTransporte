36bd38b3709365dfe57ae78105c578ef2bb52352
Luis Ballesteros <luisballesteros96@hotmail.com>
2023-02-02 19:46:57 -0500

    First commit

---
+++ src/main/java/com/example/RestApiSistemaTransporte/RestApiSistemaTransporteApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestApiSistemaTransporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSistemaTransporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiSistemaTransporteApplication.class, args);
+	}
+
+}
